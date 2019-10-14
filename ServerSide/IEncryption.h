d510645259fde408c5523ec4dafa2c8c6a19ab91
Nikita Solomientsev <araneoanicet@gmail.com>
2019-10-14 18:30:44 +0200

    add Encryptor class

--- ServerSide/IEncryption.h
+++ ServerSide/IEncryption.h
@@ -1,14 +1,42 @@
-#include <string>
+#include <unistd.h>
+#include <string.h>
 
 class IEncryption {
 protected:
-    std::string result;
+    char* result;
+    char* salt;
 public:
-    virtual void encrypt() = 0;
-    virtual bool verify() = 0;
-    virtual std::string getEncrypted() = 0;
+    virtual void encrypt(char* message) = 0;
+    virtual bool verify(char* inputMessage) = 0;
+    virtual void setSalt(char* salt) = 0;
+    virtual char* getEncrypted() = 0;
 };
 
-class SHA256Encryptor: public IEncryption {
-    // data
+class DefaultEncryptor: public IEncryption {
+private:
+    
+    char commands[4][9] = {"forward", "left", "right", "backward"};
+public:
+
+    void setSalt(char* salt) override {
+        this->salt = salt;
+    }
+
+    void encrypt(char* message) override {
+        this->result = crypt(message, this->salt);
+    }
+
+    bool verify(char* inputMessage) override {
+        
+        for (int i = 0; i < 4; i++) {
+            if (strcmp(crypt(this->commands[i], this->salt), inputMessage) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    char* getEncrypted() override {
+        return this->result;
+    }
 };
