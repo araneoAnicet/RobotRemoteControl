52e6f663fc5e773860f05f8508d92c28b60621c5
Nikita Solomientsev <araneoanicet@gmail.com>
2019-10-14 18:38:47 +0200

    add Printing output class

--- ServerSide/ICommand.h
+++ ServerSide/ICommand.h
@@ -1,6 +1,13 @@
 #include <string>
+#include <iostream>
+
 
 class ICommand {
     virtual void execute(std::string input) = 0;
 };
 
+class PrintOutput: public ICommand {
+    void execute(std::string input) override {
+        std::cout << input << std::endl;
+    }
+};
